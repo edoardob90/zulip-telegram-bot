44a4f41bc7478f2423301e11768d8889b9d8a956
edoardob90 <edoardob90@gmail.com>
2021-04-22 17:39:23 +0200

    Handles messages with attachments The attachment's link relies on a private link from Telegram API. As it exposes the bot's token, it might not be the best way to forward media content. A `dowload_file` method is implemented but not used.

--- zulip_bot.py
+++ zulip_bot.py
@@ -17,14 +17,164 @@ import os
 import sys
 import logging
 import datetime
+from typing import Any, Union, List, IO, Text, Dict, Optional
 from dateutil import tz
 from configparser import ConfigParser
 from argparse import ArgumentParser
 
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, File, Message
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 import zulip
+
+# Log an event and save it in a file with current date as name if enabled
+def log(severity, msg):
+    # Check if logging is enabled
+    if log_level == 0:
+        return
+
+    # Add file handler to logger if enabled
+    if config["log"].getboolean("log_to_file"):
+        now = datetime.datetime.now(tz=local_tz).strftime(date_format)
+
+        # If current date not the same as initial one, create new FileHandler
+        if str(now) != str(date):
+            # Remove old handlers
+            for hdlr in logger.handlers[:]:
+                logger.removeHandler(hdlr)
+
+            new_hdlr = logging.FileHandler(logfile_path, encoding="utf-8")
+            new_hdlr.setLevel(log_level)
+
+            # Format file handler
+            new_hdlr.setFormatter(formatter)
+
+            # Add file handler to logger
+            logger.addHandler(new_hdlr)
+
+    # The actual logging
+    logger.log(severity, msg)
+
+################################################################################################################
+# Zulip
+################################################################################################################
+
+def build_request(stream: Text, topic: Union[Text, datetime.datetime], content: List[Message], attachment_url: Optional[Text] = None) -> Dict:
+    """Construct the request dict to send to Zulip"""
+    request = {
+        "type": "stream",
+        "to": stream,
+        "topic": topic.strftime(date_fmt) if date_as_topic else topic,
+        "content": ""
+    }
+
+    # Message timestamp
+    date = content[0].date.astimezone(local_tz)
+
+    # Check if content represents a message with a reply
+    if content[1] is None:
+        # content = simple message, no attachments
+        if content[0].caption:
+            text = content[0].caption
+        elif content[0].text:
+            text = content[0].text
+        else:
+            text = ""
+        request['content'] = f"*{content[0].from_user.first_name}:*\n{text}"
+    else:
+        # content = reply message + original message 
+        # Check if original message and reply have the same date
+        # If not, include the date in the quoted reply
+        reply_date = content[1].date.astimezone(local_tz)
+        reply_date_print = reply_date.strftime(time_fmt) if (reply_date.strftime(date_fmt) == date.strftime(date_fmt)) else reply_date.strftime(f"{date_fmt}, {time_fmt}")
+
+        text, original_text = [x if x is not None else "" for x in [c.caption if c.caption else c.text for c in content]]
+
+        request['content'] = f"> *{content[0].from_user.first_name} wrote ({reply_date_print}):*\n> {original_text}\n\n*{content[0].from_user.first_name}:*\n{text}"
+
+    # Append a link to the attached file to the message being forwarded
+    if attachment_url is not None:
+        request['content'] += f"\n[Link to file]({attachment_url})" 
+
+    return request
+
+def submit_request(request: Dict) -> None:
+    """Submit the request & check the response JSON"""
+    if not request:
+        log(logging.ERROR, "Empty request to Zulip API ignored")
+        return
+    result = zulip_client.send_message(request)
+    if result['result'] != 'success':
+        log(logging.ERROR, f"Zulip API returned an error: {result['code']} - {result['msg']}")
+
+################################################################################################################
+# Telegram bot
+################################################################################################################
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def download_file(file: File) -> Text:
+    """Request the download of a file"""
+    file_path = os.path.join(downloads_dir, file.file_unique_id)
+    with open(file_path, 'wb') as out:
+        file.download(custom_path=None, out=out)
+    return file_path
+
+def process_message(update: Update, context: CallbackContext) -> None:
+    """Process an update message: text-only or with a media (photo, video, document, audio)"""
+    message = update.effective_message
+    user = message.from_user
+    #caption = message.caption_html if message.caption else None
+
+    # Is the message a reply?
+    original_msg = update.message.reply_to_message if message.reply_to_message else None
+
+    if message.text:
+        # text-only message
+        request = build_request(stream = stream, topic = topic, content = [message, original_msg])
+        submit_request(request)
+    else:
+        # the message has some content: photo, generic file, video, or audio are supported
+        if message.photo:
+            file_id = message.photo[-1].file_id
+        elif message.document:
+            file_id = message.document.file_id
+        elif message.video:
+            file_id = message.video.file_id
+        elif message.video_note:
+            file_id = message.video_note.file_id
+        elif message.audio:
+            file_id = message.audio.file_id
+        elif message.voice:
+            file_id = message.voice.file_id
+        else:
+            file_id = None
+            log(logging.WARNING, f"User {user} sent a message with an unsupported content")
+            text = f"Sorry {user.first_name}, I cannot forward a message with this content to Zulip 😞"
+            message.reply_text(text=text, quote=True, disable_notification=True)
+        
+        # Download the file and build a request with the file attached
+        if file_id is not None:
+            file_path = (context.bot.get_file(file_id)).file_path 
+            request = build_request(stream = stream, topic = topic, content = [message, original_msg], attachment_url = file_path)
+            submit_request(request)
+    
+
+################################################################################################################
+# Main                                                                  
+################################################################################################################
 
 # Argument parser
 ap = ArgumentParser()
@@ -47,7 +197,6 @@ config = ConfigParser()
 # Read configuration
 with open(config_file) as config_fp:
     config.read_file(config_fp)
-
 
 # Local time-zone. Use 'Europe/Zurich'
 local_tz = tz.gettz("Europe/Zurich")
@@ -78,8 +227,10 @@ if config["log"].getboolean("log_to_file"):
         log_dir = os.path.abspath(os.path.join(os.getcwd(), "logs"))
     
     # If log directory doesn't exist, create it
-    if not os.path.exists(log_dir):
+    try:
         os.makedirs(log_dir)
+    except (FileExistsError, PermissionError):
+        log(logging.ERROR, f"Directory {log_dir} already exists! Or some 'PermissionError' occurred")
 
     # Create a file handler for logging
     logfile_path = os.path.join(log_dir, date + ".log")
@@ -96,115 +247,6 @@ if config["log"].getboolean("log_to_file"):
     # Redirect all uncaught exceptions to logfile
     sys.stderr = open(logfile_path, "w")
 
-# Log an event and save it in a file with current date as name if enabled
-def log(severity, msg):
-    # Check if logging is enabled
-    if log_level == 0:
-        return
-
-    # Add file handler to logger if enabled
-    if config["log"].getboolean("log_to_file"):
-        now = datetime.datetime.now(tz=local_tz).strftime(date_format)
-
-        # If current date not the same as initial one, create new FileHandler
-        if str(now) != str(date):
-            # Remove old handlers
-            for hdlr in logger.handlers[:]:
-                logger.removeHandler(hdlr)
-
-            new_hdlr = logging.FileHandler(logfile_path, encoding="utf-8")
-            new_hdlr.setLevel(log_level)
-
-            # Format file handler
-            new_hdlr.setFormatter(formatter)
-
-            # Add file handler to logger
-            logger.addHandler(new_hdlr)
-
-    # The actual logging
-    logger.log(severity, msg)
-
-# Zulip
-# Check the response JSON of the API call
-def check_result(result):
-    if result['result'] != 'success':
-        log(logging.ERROR, f"Zulip API returned an error: {result['code']} - {result['msg']}")
-
-# Get stream & topic where to forward the message
-stream = config['zulip']['stream']
-topic = config['zulip']['to']
-date_as_topic = True if not topic else False
-
-
-# Telegram bot
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-def start(update: Update, _: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
-
-def help_command(update: Update, _: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def forward_text(update: Update, _: CallbackContext) -> None:
-    user = update.message.from_user
-    date = update.message.date.astimezone(local_tz)
-
-    log(logging.INFO, f"Forwarwding message '{update.message.text}' of user {user} received at {date.strftime(date_log_format)}")
-    
-    # Build an API request
-    request = {
-        "type": "stream",
-        "to": stream,
-        "topic": date.strftime(date_fmt) if date_as_topic else topic,
-        "content": f"*{user.first_name}:*\n{update.message.text}"
-    }
-    
-    # Process API request
-    result = zulip_client.send_message(request)
-
-    check_result(result)
-
-def forward_reply(update: Update, _: CallbackContext):
-    # The replying message
-    user = update.message.from_user
-    msg = update.message
-    date = msg.date.astimezone(local_tz)
-    
-    # The message being replied to
-    reply_msg = update.message.reply_to_message
-    reply_to_user = reply_msg.from_user
-    reply_date = reply_msg.date.astimezone(local_tz)
-    
-    # Check if original message and reply have the same date
-    # If not, include the date in the quoted reply
-    reply_date_print = reply_date.strftime(time_fmt) if (reply_date.strftime(date_fmt) == date.strftime(date_fmt)) else reply_date.strftime(f"{date_fmt}, {time_fmt}")
-
-    log(logging.INFO, f"Reply to {reply_to_user} at {reply_date.strftime(date_log_format)}\nForwarwding message '{msg.text}' of user {user} received at {date.strftime(date_log_format)}")
-
-    # Build the message content for Zulip
-    content = f"> *{reply_to_user.first_name} wrote ({reply_date_print}):*\n> {reply_msg.text}\n\n*{user.first_name}:*\n{msg.text}"
-
-    # Build an API request
-    request = {
-        "type": "stream",
-        "to": stream,
-        "topic": date.strftime(date_fmt) if date_as_topic else topic,
-        "content": content
-    }
-    
-    # Process API request
-    result = zulip_client.send_message(request)
-
-    check_result(result)
-
-################################################################################################################
-
 # Set up Zulip API
 api_key, email, site = config["zulip"]["key"], config["zulip"]["email"], config["zulip"]["site"]
 if None in (api_key, email):
@@ -213,6 +255,21 @@ if None in (api_key, email):
     exit(msg)
 else:
     zulip_client = zulip.Client(api_key=api_key, email=email, site=site)
+
+# Get stream & topic where to forward the message
+stream = config['zulip']['stream']
+topic = config['zulip']['to']
+# By default, use message date as the topic
+date_as_topic = True if not topic else False
+
+# Create the directory where to download files requested to Telegram
+# Downloads dir is by default in $PWD/telegram_downloads
+# TODO: implement something to purge this folder when the bot stops or restarts
+downloads_dir = os.path.join(os.getcwd(), 'telegram_downloads')
+try:
+    os.makedirs(downloads_dir)
+except (FileExistsError, PermissionError):
+    log(logging.ERROR, f"Directory {downloads_dir} exists!")
     
 # Create the Updater and pass it your bot's token.
 updater = Updater(config["telegram"]['bot_token'])
@@ -225,8 +282,7 @@ dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("help", help_command))
 
 # on non command i.e message - echo the message on Telegram
-dispatcher.add_handler(MessageHandler(~Filters.command & Filters.reply, forward_reply))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_text))
+dispatcher.add_handler(MessageHandler(Filters.all & ~Filters.command, process_message))
 
 # Start the Bot
 updater.start_polling()
